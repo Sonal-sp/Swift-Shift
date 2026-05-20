78fccca08aaf2d783c202a963ae81a6789bf20bc
Sonal-sp <sonalparmar2697@gmail.com>
2026-05-20 11:39:43 +0530

    Fixed the error.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navbar from './components/Navbar';
+import NavBar from './components/NavBar';
 import Hero from './components/Hero';
 import Services from './components/Services';
 import FAQ from './components/FAQ';
